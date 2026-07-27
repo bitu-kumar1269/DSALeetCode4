76600ea1ca3732bfb8c21b37f9a7fde338733e9e
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-07-27 20:25:42 +0530

    Time: 9 ms (98.12%) | Memory: 59.2 MB (88.17%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,21 +10,50 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();
+        if(head == null || head.next == null){
+            return head;
+        }
 
-        ListNode temp = head;
-        while( temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-        Collections.sort(list);
-        temp = head;
-        int i=0;
-        while(temp != null){
-            temp.val = list.get(i);
-            i++;
-            temp = temp.next;
-        }
-        return head;
+        ListNode mid = getMid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null; // disconnect two halfs
+
+        // recursively sort each half
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        // merge two sorted halfs
+        return merge(left, right);
     }
+    private ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next; // fast start 1 step more so that slow end at mid
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        // attach remainin nodes if any left
+        curr.next = (l1 != null) ? l1: l2;
+
+        return dummy.next;
+
+    }
+
 }
