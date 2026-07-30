17dab13ca2857f628c2c341cdc8136044cabf6c5
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-07-30 12:05:05 +0530

    Time: 1 ms (85.47%) | Memory: 42.9 MB (39.92%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -4,31 +4,32 @@ class Solution {
         Stack<StringBuilder> stack = new Stack<>();
 
         StringBuilder str = new StringBuilder();
-        int k = 0;
+        int digit = 0;
 
         for(char ch: s.toCharArray()){
             if(Character.isDigit(ch)){
-                k = k * 10 +(ch -'0');
+                digit= digit * 10 + (ch-'0');
             }
-            else if(ch =='['){
-                count.push(k);
+            else if(ch == '['){
+                count.push(digit);
                 stack.push(str);
-
                 str = new StringBuilder();
-                k=0;
-            }
-            else if(ch == ']'){
-                int repeat = count.pop();
-                StringBuilder prevStr = stack.pop();
+                digit = 0;
 
-                while(repeat > 0){
-                    prevStr.append(str);
-                    repeat--;
+            }
+            else if(ch ==']'){
+                int num = count.pop();
+                StringBuilder newstr = stack.pop();
+
+                while(num > 0){
+                    newstr.append(str);
+                    num--;
                 }
-                str = prevStr;
-            }else{
+                str = newstr;
+            }
+            else{
                 str.append(ch);
-            }    
+            }
         }
         return str.toString();
 
