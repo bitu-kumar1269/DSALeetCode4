44b4967f21cf397ec5c2081823b81b7928a23cc6
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-07-31 15:19:15 +0530

    Time: 1584 ms (15.44%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select u.user_id as buyer_id, u.join_date, count(o.order_id) as orders_in_2019
+select 
+u.user_id as buyer_id, 
+u.join_date, 
+count(o.order_id) as orders_in_2019 
 from Users u
-left join Orders o on u.user_id = o.buyer_id
-and o.order_date >= '2019-01-01'
-and o.order_date <= '2019-12-31'
-group by u.user_id , u.join_date;
+left join Orders o 
+on u.user_id = o.buyer_id and year(o.order_date) ='2019'
+-- and o.order_date >= '2019-01-01'
+-- and o.order_date <= '2019-12-31'
+group by u.user_id;
