2327e8e18d72806cd4bf71328c73024ce2b95c2b
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-08-01 08:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (89.58%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,26 +1,17 @@
 class Solution {
-    private int solve(int i, int j, int nums[]){
-        if(i > j){
-            return 0;
-        }
-        if( i == j){
-            return nums[i];
-        }
-        int take_i = nums[i] + Math.min(solve(i+2, j, nums), solve(i+1, j-1, nums));
-        int take_j = nums[j] + Math.min(solve(i, j-2, nums), solve(i+1, j-1, nums));
 
-        return Math.max(take_i, take_j);
-    }
     public boolean predictTheWinner(int[] nums) {
+        
         int n = nums.length;
-        int total_score = 0;
-        for(int num: nums){
-            total_score += num;
+        int[] dp = nums.clone(); 
+        
+        for (int diff = 1; diff < n; ++diff) {
+            for (int j = n - 1; j - diff >= 0; --j) {
+                int i = j - diff;
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
         }
-
-        int player1_score = solve(0, n-1, nums);
-        int player2_score = total_score - player1_score;
-
-        return player1_score >= player2_score;
+        
+        return dp[n - 1] >= 0;
     }
 }
