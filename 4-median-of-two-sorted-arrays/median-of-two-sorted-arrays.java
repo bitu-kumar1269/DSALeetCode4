243af153979d0c4d712f08f160b00b478c36bb8f
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-08-02 09:45:30 +0530

    Time: 1 ms (100.00%) | Memory: 49.1 MB (16.14%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,32 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-
+        int n1 = nums1.length, n2 = nums2.length;
         int[] temp = new int[n1+n2];
         int i=0, j=0, k=0;
 
         while(i < n1 && j < n2){
-            if(nums1[i] <= nums2[j]){
+            if(nums1[i] <= nums2[j])
                 temp[k++] = nums1[i++];
-            }else{
+            else
                 temp[k++] = nums2[j++];
-            }
-        }
-        while(i<n1){
-            temp[k++] = nums1[i++];
-        }
-        while(j<n2){
-            temp[k++] = nums2[j++];
         }
         
+        // 2. Copy remaining elements of nums1, if any
+        while(i<n1) temp[k++] = nums1[i++];
+        // 3. Copy remaining elements of nums2, if any
+        while(j<n2) temp[k++] = nums2[j++];
+        
+        // 4. Calculate Median
         int len = temp.length;
-        double ans = 0;
-        if(len % 2 ==0){
-            ans = (temp[len /2] + temp[(len /2) -1])/2.0;
-        }else{
-            ans = temp[len / 2];
-        }
-        return ans;
+        if(len % 2 ==0){ // for even len -> (2 + 3) / 2 = 2.5.
+            return (temp[len /2 -1] + temp[len /2])/2.0;
+        }//else
+        return temp[len / 2];
     }
 }
