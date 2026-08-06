67fa331acb664fee63e8fcac6f45847668b90299
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-08-06 16:03:32 +0530

    Time: 18 ms (86.22%) | Memory: 196.8 MB (15.59%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,39 +1,44 @@
-import java.util.*;
-
 class Solution {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    Boolean[][] dp;
+
     public boolean canCross(int[] stones) {
-        // Base case: The frog starts at 0 and MUST make a jump of 1 unit to reach stones[1].
-        if (stones[1] != 1) return false;
 
         int n = stones.length;
 
-        // Map each stone position to a set of valid incoming jump sizes 'k'
-        Map<Integer, Set<Integer>> dp = new HashMap<>();
-        for (int stone : stones) {
-            dp.put(stone, new HashSet<>());
+        for (int i = 0; i < n; i++) {
+            map.put(stones[i], i);
         }
 
-        // Base state: At stone 0, initial jump size is 0
-        dp.get(0).add(0);
+        dp = new Boolean[n][n + 1];
 
-        // Process each stone
-        for (int i = 0; i < n; i++) {
-            int currentStone = stones[i];
+        return solve(stones, 0, 0);
+    }
 
-            // For every jump length 'k' that can reach 'currentStone'
-            for (int k : dp.get(currentStone)) {
-                
-                // The frog can choose a next step size of k-1, k, or k+1
-                for (int step = k - 1; step <= k + 1; step++) {
-                    // Step must be positive (> 0) and target stone must exist in our map
-                    if (step > 0 && dp.containsKey(currentStone + step)) {
-                        dp.get(currentStone + step).add(step);
-                    }
+    public boolean solve(int[] stones, int idx, int jump) {
+
+        if (idx == stones.length - 1)
+            return true;
+
+        if (dp[idx][jump] != null)
+            return dp[idx][jump];
+
+        for (int nextJump = jump - 1; nextJump <= jump + 1; nextJump++) {
+
+            if (nextJump <= 0)
+                continue;
+
+            int nextStone = stones[idx] + nextJump;
+
+            if (map.containsKey(nextStone)) {
+
+                if (solve(stones, map.get(nextStone), nextJump)) {
+                    return dp[idx][jump] = true;
                 }
             }
         }
 
-        // If the set for the last stone contains any jump sizes, we can reach it!
-        return !dp.get(stones[n - 1]).isEmpty();
+        return dp[idx][jump] = false;
     }
 }
