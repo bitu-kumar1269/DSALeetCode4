acd49eda492b3a94feac378057965766f349deed
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-08-12 10:20:39 +0530

    Time: 66 ms (74.64%) | Memory: 89.1 MB (49.96%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
         int n = nums.length;
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        int maxLength = 0, left = 0;
+        // i use hashmap to count freq of each element 
+        HashMap<Integer,Integer> freq = new HashMap<>(); 
+        int maxLength = 0, left = 0; // max store maxlengthsubarray, and left for reduse when freq of nums[i] grater k
         
         for(int right = 0; right < n; right++){
             int num = nums[right];
             freq.put(num, freq.getOrDefault(num, 0)+ 1);
-
+            // it will only excute when freq of element is > k(2)
             while(freq.get(num) > k){
                 int leftnum = nums[left];
+                
                 freq.put(leftnum, freq.get(leftnum)- 1);
                 left++;
             }
