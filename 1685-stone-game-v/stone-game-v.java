6f78314de6470d7648e4568a53502c8395a952d0
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-08-17 11:25:55 +0530

    Time: 303 ms (25.96%) | Memory: 47.5 MB (76.68%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + stoneValue[i];
+
+        int[][] dp = new int[n][n];
+
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i + length - 1 < n; i++) {
+                int j = i + length - 1;
+                int best = 0;
+                for (int k = i; k < j; k++) {
+                    int left = prefix[k + 1] - prefix[i];
+                    int right = prefix[j + 1] - prefix[k + 1];
+                    if (left < right) {
+                        best = Math.max(best, left + dp[i][k]);
+                    } else if (left > right) {
+                        best = Math.max(best, right + dp[k + 1][j]);
+                    } else {
+                        best = Math.max(best, left + Math.max(dp[i][k], dp[k + 1][j]));
+                    }
+                }
+                dp[i][j] = best;
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
