9295ee80ae973f340ff310540eae8369184ccf91
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-09-24 18:23:20 +0530

    Time: 339 ms (91.87%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 select e1.name
 from Employee e1
-join Employee e2 on e1.id = e2.managerId
+-- self join again with different notation
+join Employee e2  
+-- e1.id is same e2.managerId common
+on e1.id = e2.managerId
+-- make group of return column
 group by e1.id, e1.name
+-- fillter e2 id which more than equal to appear in managerId column
 having count(e2.id) >= 5;
